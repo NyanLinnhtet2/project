32fd93b484c63f80c4019a43b152bbfde9060607
La Min Hein <dioxidecarbondioxide@gmail.com>
2026-08-03 22:57:49 +0630

    Fix type assertion for transporter in mailer

--- server/src/utils/mailer.ts
+++ server/src/utils/mailer.ts
@@ -25,7 +25,7 @@ const getTransporter = (): Transporter | null => {
     secure: Number(SMTP_PORT) === 465,
     family: 4,
     auth: { user: SMTP_USER, pass: SMTP_PASS },
-  });
+  } as any);
   return transporter;
 };
 

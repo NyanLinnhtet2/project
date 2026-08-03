80f14ac1e81ec2aaff0c6fb98cb3f18830c42d41
La Min Hein <dioxidecarbondioxide@gmail.com>
2026-08-03 22:36:39 +0630

    Add IPv4 family option to mailer transport

--- server/src/utils/mailer.ts
+++ server/src/utils/mailer.ts
@@ -23,6 +23,7 @@ const getTransporter = (): Transporter | null => {
     host: SMTP_HOST,
     port: Number(SMTP_PORT) || 587,
     secure: Number(SMTP_PORT) === 465,
+    family: 4,
     auth: { user: SMTP_USER, pass: SMTP_PASS },
   });
   return transporter;
